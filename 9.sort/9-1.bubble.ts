032344e5c165cc42d214787581e7386cf1d06293
陈贽 <chenzhi2017@xinhua.org>
2021-03-19 17:59:15 +0800

    update

--- 9.sort/9-1.bubble.ts
+++ 9.sort/9-1.bubble.ts
@@ -17,7 +17,24 @@ const bubbleSort = (arr: number[]): void => {
     }
   }
 };
+const bubbleSort2 = (arr: number[]): void => {
+  let sorted: boolean = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (sorted === true) break;
+    sorted = true;
+    for (let j = arr.length - 1; j > i; j--) {
+      if (arr[j] >= arr[j - 1]) continue;
+      swap(arr, j, j - 1);
+      sorted = false;
+    }
+  }
+};
 const arr = init();
 console.log(arr);
 bubbleSort(arr);
 console.log(arr);
+
+const arr2 = init();
+console.log(arr2);
+bubbleSort2(arr2);
+console.log(arr2);
